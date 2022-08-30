c41d187c3545b728786248f1d07be74cdc99eece
inuma <yusukennh@gmail.com>
2022-08-30 22:31:06 +0900

    the today's draft

--- generator/header-generator.lisp
+++ generator/header-generator.lisp
@@ -3,11 +3,11 @@
         <meta charset=\"utf-8\">
         <meta property=\"og:site_name\" content=\"日記\" />
         <meta property=\"og:description\" content=\"日記\" />
-        <meta property=\"og:image\" content=\"https://diary.199024.club/test.jpg\" />
+        <meta property=\"og:image\" content=\"https://199024.club/test.jpg\" />
         <meta property=\"og:title\" content=\"日記\"/>
         <meta property=\"twitter:card\" content=\"summary\" />
-        <meta property=\"twitter:image\" content=\"https://diary.199024.club/test.jpg\" />
-        <link rel=\"icon\" href=\"https://diary.199024.club/test.jpg\">
+        <meta property=\"twitter:image\" content=\"https://199024.club/test.jpg\" />
+        <link rel=\"icon\" href=\"https://199024.club/test.jpg\">
         <title>日記</title>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
     "
